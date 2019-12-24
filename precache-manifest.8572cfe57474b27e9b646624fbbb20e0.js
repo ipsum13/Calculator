fba9040fdfaa64215734283b88b2e58d7c4b9368
ipsum13 <samiul.lesum@outlook.com>
2019-12-24 04:06:36 -0600

    Updates

---
+++ precache-manifest.8572cfe57474b27e9b646624fbbb20e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934d38b95a067f243452cd27b895b274",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "a92e9d018f0dad136f35",
+    "url": "/Calculator/static/css/main.140203f0.chunk.css"
+  },
+  {
+    "revision": "ba660f566e8a47f5911e",
+    "url": "/Calculator/static/js/2.e968c221.chunk.js"
+  },
+  {
+    "revision": "a92e9d018f0dad136f35",
+    "url": "/Calculator/static/js/main.27f2a0d0.chunk.js"
+  },
+  {
+    "revision": "948835a5ae24ee819954",
+    "url": "/Calculator/static/js/runtime-main.2f606aad.js"
+  }
+]);
